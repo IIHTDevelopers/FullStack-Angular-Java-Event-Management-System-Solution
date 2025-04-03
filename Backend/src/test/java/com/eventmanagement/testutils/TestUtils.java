f5b593b030c1a22fae200fdba6b9073edfc63a8a
AkshayTechademy <Akshay.Deshmukh@techademy.net>
2025-04-03 12:52:56 +0530

    🔄 Updated 2 test files

--- Backend/src/test/java/com/eventmanagement/testutils/TestUtils.java
+++ Backend/src/test/java/com/eventmanagement/testutils/TestUtils.java
@@ -77,7 +77,11 @@ public class TestUtils {
 		TestResults testResults = new TestResults();
 		Map<String, TestCaseResultDto> testCaseResults = new HashMap<String, TestCaseResultDto>();
 
-		customData = readData("../custom.ih");
+        customData = "";
+        try{
+		    customData = readData("../custom.ih");
+		}catch(Exception e)	{}
+
 		String resultStatus = "Failed";
 		int resultScore = 0;
 		if (result.toString().equals("true")) {
